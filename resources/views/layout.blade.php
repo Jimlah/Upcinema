9ab9c035a6ff0f48dc3af5eff56a947be50df0d1
Abdullahi <46778885+Jimlah@users.noreply.github.com>
2021-01-05 17:25:50 +0100

    layout feature created

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,10 +7,28 @@
     <title>@yield('title', 'Upcinema')</title>
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
 </head>
-<body>
+<body class="bg-gray-800 text-gray-300">
 
-    <div class="bg-gray-700">Hello World</div>
+    <nav class="flex space-x-2 py-3 px-1 bg-gray-900 container justify-self-center sticky top-0">
+        <a href="/" class="flex-none">Upcinema</a>
+        <form class="flex flex-auto bg-white">
+            <input type="search" class="flex-auto">
+            <button type="submit" class="">
+                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path>
+                </svg>
+            </button>
+        </form>
+        <div class="flex-none flex space-x-1">
+            <a href="/register">Register</a>
+            <a href="/login">Login</a>
+        </div>
+    </nav>
 
     @yield('content')
+
+    <footer class="flex space-x-2 fixed bottom-0 py-3 px-1 bg-gray-900 container justify-self-center">
+        <p class="text-center">&copy; 2015 RapidTables.com<p>
+    </footer>
 </body>
 </html>
